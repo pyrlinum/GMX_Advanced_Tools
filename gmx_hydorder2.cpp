dc9b4c49e2db4cbfba22068d34812c16838865f7
pyrlinum <73190310+pyrlinum@users.noreply.github.com>
2025-08-22 12:23:00 +0100

    Update gmx_hydorder2.cpp

--- gmx_hydorder2.cpp
+++ gmx_hydorder2.cpp
@@ -1,36 +1,11 @@
-/*
- * This file is part of the GROMACS molecular simulation package.
- *
- * Copyright 2010- The GROMACS Authors
- * and the project initiators Erik Lindahl, Berk Hess and David van der Spoel.
- * Consult the AUTHORS/COPYING files and https://www.gromacs.org for details.
- *
- * GROMACS is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- *
- * GROMACS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with GROMACS; if not, see
- * https://www.gnu.org/licenses, or write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
- *
- * If you want to redistribute modifications to GROMACS, please
- * consider that scientific software is very special. Version
- * control is crucial - bugs must be traceable. We will be happy to
- * consider code for inclusion in the official distribution, but
- * derived work must not be called official GROMACS. Details are found
- * in the README & COPYING files - if they are missing, get the
- * official version at https://www.gromacs.org.
- *
- * To help us fund GROMACS development, we humbly ask that you cite
- * the research papers on the package. Check out https://www.gromacs.org.
- */
+// This file is a fork of GROMACS ver 2025 GMX HYDORDER TOOL
+// Changes with respet to orignal code:
+// This version computes frequency distribution of OTO parameter in either original or normalized form
+// over the whole box or a volume bewteen surfaces defined by lower and upper radius from 'Protein' group
+// 
+// This is an unoptimized serial version 
+
+
 
 #include "gmxpre.h"
 
